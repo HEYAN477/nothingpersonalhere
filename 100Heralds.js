ae315e08decd4922813290a2007e6cbbdcf75ae2
HEYAN477 <144469906+HEYAN477@users.noreply.github.com>
2025-10-03 19:06:26 -0300

    Update 100Heralds.js

--- 100Heralds.js
+++ 100Heralds.js
@@ -2,12 +2,12 @@ Game.registerMod("100Heralds", {
   init:function(){
     Game.heralds = 100;
     Game.playCookieClickSound=function()
-		{
-			if (Game.prefs.cookiesound) PlaySound('snd/clickb'+(Game.cookieClickSound)+'.mp3',0.5);
-			else {return};
-			Game.cookieClickSound+=Math.floor(Math.random()*4)+1;
-			if (Game.cookieClickSound>7) Game.cookieClickSound-=7;
-		}
+    {
+		if (Game.prefs.cookiesound) PlaySound('snd/clickb'+(Game.cookieClickSound)+'.mp3',0.5);
+		else {return};
+		Game.cookieClickSound+=Math.floor(Math.random()*4)+1;
+		if (Game.cookieClickSound>7) Game.cookieClickSound-=7;
+	}
     AddEvent(window,'keydown',function(e){
       //ctrl-r reloads the game
       if (!Game.OnAscend && Game.AscendTimer==0 && e.ctrlKey && e.keyCode==82 ) {
