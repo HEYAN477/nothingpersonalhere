b108a2fa1d2146ddfd37ae016f79a5ff8e4a570f
HEYAN477 <144469906+HEYAN477@users.noreply.github.com>
2025-10-03 19:03:24 -0300

    Update 100Heralds.js

--- 100Heralds.js
+++ 100Heralds.js
@@ -1,17 +1,13 @@
-Game.registerMod("removethatannoyingsound", {
+Game.registerMod("100Heralds", {
   init:function(){
     Game.heralds = 100;
-    Game.playCookieClickSound = function(){return};
-    let MOD = this;
-    MOD.OriginalTickerDraw = Game.TickerDraw
-    Game.TickerDraw = function() {
-        var str='';
-        if (Game.Ticker!='') str=Game.Ticker;
-        if (str.startsWith("News : ")){
-            Game.Ticker = "News: " + str.charAt(7).toUpperCase() + str.slice(8);
-        }
-        MOD.OriginalTickerDraw();
-    }
+    Game.playCookieClickSound=function()
+		{
+			if (Game.prefs.cookiesound) PlaySound('snd/clickb'+(Game.cookieClickSound)+'.mp3',0.5);
+			else {return};
+			Game.cookieClickSound+=Math.floor(Math.random()*4)+1;
+			if (Game.cookieClickSound>7) Game.cookieClickSound-=7;
+		}
     AddEvent(window,'keydown',function(e){
       //ctrl-r reloads the game
       if (!Game.OnAscend && Game.AscendTimer==0 && e.ctrlKey && e.keyCode==82 ) {
