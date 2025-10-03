9ee76cfd21789b8f2c2747a14c6b147114648bb4
HEYAN477 <144469906+HEYAN477@users.noreply.github.com>
2025-10-03 15:32:24 -0300

    Update RemoveCookieSound2.js

--- RemoveCookieSound2.js
+++ RemoveCookieSound2.js
@@ -12,5 +12,12 @@ Game.registerMod("removethatannoyingsound", {
         }
         MOD.OriginalTickerDraw();
     }
+    AddEvent(window,'keydown',function(e){
+      //ctrl-r reloads the game
+      if (!Game.OnAscend && Game.AscendTimer==0 && e.ctrlKey && e.keyCode==82 ) {
+        Game.toReload=true;
+        e.preventDefault();
+      }
+    });
   }
 });
